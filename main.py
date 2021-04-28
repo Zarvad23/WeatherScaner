e603a4c742c3016a154da4f0ec0e34427fd1bfce
Zarvad23 <zarifullinvadim@mail.ru>
2021-04-28 21:47:33 +0500

    3

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-import eel
-import pyowm
+import eel #подключение библиотеки веб интерфесов
+import pyowm #подключение библиотеки для взаимодействия с API OpenWeatherMap
 
-owm = pyowm.OWM("f17480b84563d8c6cde6484c071ca775")
+owm = pyowm.OWM("f17480b84563d8c6cde6484c071ca775") 
 
-@eel.expose
-def get_weather(place):
-	city = place
-	mgr = owm.weather_manager()
-
-	observation = mgr.weather_at_place(city)
-	w = observation.weather
+@eel.expose #говорим что эту функцию можно использовать с js
+def get_weather(place):#Описание функции с аргументом place = город который мы выбрали
+	city = place #присваиваем город к переменной city
+	mgr = owm.weather_manager() #присваиваем модуль api к переменной mgr
+	observation = mgr.weather_at_place(city) # присваиваем mgr+погода для города
+	w = observation.weather 
 
 	temp = w.temperature('celsius')['temp']
-
-	return "В городе " + city + " сейчас " + str(temp) + " градусов!"
+	result = "В городе " + city + " сейчас " + str(temp) + " градусов!"
+	return result
 
 #Вывод Окна веб приложения
 
-eel.init("web")
-eel.start("main.html", size=(500, 500))
+eel.init("web") #Создание окна веб приложения
+eel.start("main.html", size=(700, 700))#исполняемый html файл для окна веб приложения
