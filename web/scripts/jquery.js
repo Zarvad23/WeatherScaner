5757a42cc063b5ad39e82ceafd3f69c49b44c557
Zarvad23 <zarifullinvadim@mail.ru>
2021-05-15 18:33:08 +0500

    final

--- web/scripts/jquery.js
+++ web/scripts/jquery.js
@@ -1,8 +1,8 @@
 jQuery('#show').on('click', function(){//для удобства чтобы при нажатии кнопки запускался скрипт
-			display_weather();
-			$('#gorod').show( "slow");
-			$('#reload').show( "slow");
-			var iconcode = a.weather['10d'].icon;
-			var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-			$('#wicon').attr('src', iconurl);
-		})
+	display_weather();
+	$('#gorod').show( "slow");
+	$('#reload').show( "slow");
+	var iconcode = a.weather['10d'].icon;
+	var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+	$('#wicon').attr('src', iconurl);
+})
